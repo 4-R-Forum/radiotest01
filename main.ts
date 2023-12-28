d5d6453469a7ef45cc7dd66287c795b160150c8f
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-12-28 13:26:16 -0500

    Test send-receive successful

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 let data_string = ""
 radio.setGroup(1)
-radio.setTransmitPower(7)
+radio.setTransmitPower(2)
 radio.setFrequencyBand(0)
 let period = 5000
 loops.everyInterval(period, function () {
     data_string = "" + input.temperature() + "," + input.lightLevel()
     radio.sendString(data_string)
+    basic.showString(data_string)
 })
