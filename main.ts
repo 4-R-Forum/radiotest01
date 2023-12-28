8146d53a496a87d3a9be74c808a1a878d0c66ece
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-12-28 17:46:11 -0500

    Buttons change transmit power

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    if (tx_power < 7) {
+        tx_power += 1
+    }
+    basic.showString("P=" + convertToText(tx_power))
+})
+input.onButtonPressed(Button.B, function () {
+    if (tx_power > 0) {
+        tx_power += -1
+    }
+    basic.showString("P=" + convertToText(tx_power))
+})
 let data_string = ""
+let tx_power = 0
+let period = 5000
+tx_power = 0
 radio.setGroup(1)
 radio.setTransmitPower(2)
 radio.setFrequencyBand(0)
-let period = 5000
 loops.everyInterval(period, function () {
     data_string = "" + input.temperature() + "," + input.lightLevel()
     radio.sendString(data_string)
